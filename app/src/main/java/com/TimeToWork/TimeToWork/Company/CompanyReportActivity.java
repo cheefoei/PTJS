279f1f2006e2828e5ff81787af973153180bc82a
marstan96 <33213686+marstan96@users.noreply.github.com>
2018-11-13 20:15:06 +0800

    Add files via upload

--- app/src/main/java/com/TimeToWork/TimeToWork/Company/CompanyReportActivity.java
+++ app/src/main/java/com/TimeToWork/TimeToWork/Company/CompanyReportActivity.java
@@ -4,11 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.TimeToWork.TimeToWork.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class CompanyReportActivity extends AppCompatActivity {
+
+    private String months, years;
+    private int month, year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,23 +27,52 @@ public class CompanyReportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_report);
 
-        Button btnWorkerRatingReport = (Button) findViewById(R.id.btnReport1);
-        Button btnCompanyRatingReport = (Button) findViewById(R.id.btnReport2);
-
-        btnWorkerRatingReport.setOnClickListener(new View.OnClickListener() {
+        Spinner spinnerMonth = (Spinner) findViewById(R.id.spinner_card_month);
+        spinnerMonth.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CompanyReportActivity.this, WorkerRatingReportActivity.class);
-                startActivity(intent);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                months = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
             }
         });
 
-        btnCompanyRatingReport.setOnClickListener(new View.OnClickListener() {
+        Spinner spinnerYear = (Spinner) findViewById(R.id.spinner_card_year);
+        spinnerYear.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                years = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        Button btnShow = (Button) findViewById(R.id.btnShow);
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CompanyReportActivity.this, WorkerRatingReportActivity.class);
+
+                Date date = null;
+                try {
+                    date = new SimpleDateFormat("MMMM", Locale.ENGLISH).parse(months);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(date);
+
+                month = cal.get(Calendar.MONTH) + 1;
+                year = Integer.parseInt(years);
+
+                Intent intent = new Intent(CompanyReportActivity.this, PaymentReport.class);
+                intent.putExtra("month", month);
+                intent.putExtra("year", year);
                 startActivity(intent);
             }
         });
